26251697aba6574cd5d70030ac2df7aac4987331
Michel RABEMANANTSOA <michel.rabemanantsoa@devoteam.com>
2020-06-18 12:14:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Awesome app</title>
+  </head>
+  <body>
+  <h1>Welcome to the awesome app !</h1>
+
+  <p> App backend deployed on <?=gethostname()?> </p>
+  </body>
+</html>
+
+
